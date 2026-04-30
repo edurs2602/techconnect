49afe5395976fd232e848a16ed97c634603584dc
iantbdev <familiadogutin@gmail.com>
2026-04-30 16:24:23 -0300

    chore: main

---
+++ cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"techconnect/config"
+	httpAdapter "techconnect/internal/adapter/in/http"
+	"techconnect/internal/adapter/out/postgres"
+	"techconnect/internal/adapter/out/security"
+	"techconnect/internal/application/usecase"
+	"techconnect/internal/domain/user"
+)
+
+func main() {
+	cfg := config.Load()
+
+	hasher := security.BcryptHasher{}
+	userRepo := postgres.NewUserRepository()
+	userSvc := user.NewService(userRepo, hasher)
+	registerUC := usecase.NewRegisterUseCase(userSvc)
+
+	userHandler := httpAdapter.NewUserHandler(registerUC)
+	router := httpAdapter.NewRouter(userHandler)
+
+	addr := fmt.Sprintf(":%s", cfg.Port)
+	log.Printf("servidor rodando em %s", addr)
+	log.Fatal(http.ListenAndServe(addr, router))
+}
